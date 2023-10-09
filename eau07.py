89acb6e6f4f6c3248d5a2c52884fe564cf04256c
Marie <mari.plisson@gmail.com>
2023-10-09 09:10:32 +0200

    EPREUVE_EAU

--- eau07.py
+++ eau07.py
@@ -2,20 +2,25 @@
 
 import sys
 
-string = sys.argv[1]
-result = ""
+try: 
+    string = sys.argv[1]
+    result = ""
 
-def upper_char(char):
-    if 'a' <= char <= 'z':
-        return chr(ord(char) - 32)
-    return char
+    def upper_char(char):
+        if 'a' <= char <= 'z':
+            return chr(ord(char) - 32)
+        return char
 
- #méthode unpacking, extraire le premier mot de la chaîne, rassembler tous les mots restants dans une liste
- #découper la chaîne en une liste de mots, effectuer qu'une seule découpe
-while len(string):
-    first_word, *rest_of_string = string.split(maxsplit=1)
-    result += upper_char(first_word[0]) + first_word[1:] + " "
-    string = rest_of_string[0] if rest_of_string else ""
+    #méthode unpacking, extraire le premier mot de la chaîne, rassembler tous les mots restants dans une liste
+    #découper la chaîne en une liste de mots, effectuer qu'une seule découpe
+    while len(string):
+        first_word, *rest_of_string = string.split(maxsplit=1)
+        result += upper_char(first_word[0]) + first_word[1:] + " "
+        string = rest_of_string[0] if rest_of_string else ""
 
-print(result.strip())
+    print(result.strip())
+except:
+    print("error")
+
+
 
